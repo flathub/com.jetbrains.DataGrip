528c8514c61d57ca9aa537744e3bfba8f974a034
x80486 <masked.github@mailnull.com>
2020-07-01 11:35:08 -0400

    chore: datagrip flatpak

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 
 set -o errexit
 
-JAVA_TOOL_OPTIONS=-Djava.io.tmpdir=${XDG_CACHE_HOME}/tmp/
+DATAGRIP_JDK="/app/extra/datagrip/jre64"
+export DATAGRIP_JDK
+JAVA_TOOL_OPTIONS="-Djava.io.tmpdir=${XDG_CACHE_HOME}/tmp"
 export JAVA_TOOL_OPTIONS
-TMPDIR=${XDG_CACHE_HOME}/tmp/
+TMPDIR="${XDG_CACHE_HOME}/tmp"
 export TMPDIR
 
 exec env /app/extra/datagrip/bin/datagrip.sh "$@"
