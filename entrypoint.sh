5fb50f2afadbcd5bdb4377ccd52d57883f3dd82a
Junjie Yuan <junjieraw@gmail.com>
2020-08-28 22:56:12 +0800

    delete unused JAVA_TOOL_OPTIONS environment variable
    Signed-off-by: Junjie Yuan <junjieraw@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,7 @@ set -o errexit
 
 DATAGRIP_JDK="/app/extra/datagrip/jre64"
 export DATAGRIP_JDK
-JAVA_TOOL_OPTIONS="-Djava.io.tmpdir=${XDG_CACHE_HOME}/tmp"
-export JAVA_TOOL_OPTIONS
-TMPDIR="${XDG_CACHE_HOME}/tmp"
+TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
 export TMPDIR
 
 exec env /app/extra/datagrip/bin/datagrip.sh "$@"
