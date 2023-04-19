4ebe399c3c3be9ed0a1efc433f6fb62df0546673
x80486 <gfd2zs93@duck.com>
2023-04-18 20:37:10 -0400

    Update shell scripts to make use of bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -o errexit
 
 DATAGRIP_JDK="/app/extra/datagrip/jre64"
 export DATAGRIP_JDK
-TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
 export TMPDIR
 
 exec env zypak-wrapper /app/extra/datagrip/bin/datagrip.sh "$@"
