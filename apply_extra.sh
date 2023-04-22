3d2bd1c189dae702db2aabcaed42aa5e530ee2e1
x80486 <gfd2zs93@duck.com>
2023-04-22 17:01:05 -0400

    Remove zypak-wrapper and set property to avoid issues with Chrome's sandboxing mechanism

--- apply_extra.sh
+++ apply_extra.sh
@@ -4,4 +4,5 @@ set -o errexit
 
 install --directory --mode=0755 datagrip/
 tar --directory=datagrip/ --extract --file=datagrip.tar.gz --gunzip --strip-components=1
+tee < idea.properties --append datagrip/bin/idea.properties
 rm --force datagrip.tar.gz
