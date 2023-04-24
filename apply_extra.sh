cf5851ec0f7d11236e358543ef86716261d9dc56
Ryan T. Moran <ryan@rtmoran.org>
2023-04-24 13:16:36 -0400

    Moved idea.properties definition to manifest

--- apply_extra.sh
+++ apply_extra.sh
@@ -4,5 +4,4 @@ set -o errexit
 
 install --directory --mode=0755 datagrip/
 tar --directory=datagrip/ --extract --file=datagrip.tar.gz --gunzip --strip-components=1
-tee < idea.properties --append datagrip/bin/idea.properties
 rm --force datagrip.tar.gz
